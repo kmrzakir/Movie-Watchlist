17220c5a0164910e103ab8fc5f20db88d1c75de0
zakir <zakirmustafa444@gmail.com>
2025-02-12 21:51:00 +0530

    added watch list

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  darkMode: "class", // Enables manual dark mode toggle
+  content: ["./*.html"], // Ensures Tailwind scans all HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
